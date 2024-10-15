0faf024c74a7f2f09bc97468983f0cc853b60946
tanzimjarif <tanzim.jarif63@gmail.com>
2024-10-15 18:41:55 +0100

    updated tracker

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,12 @@
+from zenml.client import Client
+
 from pipelines.basic_pipeline import forecast_pipeline
 
+
 if __name__ == "__main__":
+
+    #printing the tracker
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
+
     #run the pipeline
     forecast_pipeline()
